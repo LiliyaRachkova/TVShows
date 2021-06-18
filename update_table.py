e564af3cb8763871e44a2e1974c3315644c572a3
LiliyaRachkova <lili.rachkova@ya.ru>
2021-06-18 11:14:01 +0300

    divided into different files

--- update_table.py
+++ update_table.py
@@ -1,21 +1,8 @@
 import requests
 import psycopg2
 import dateparser
-import pandas as pd
-import numpy as np
 
 TVSHOW = "http://api.tvmaze.com/search/shows?q="
-
-
-def print_table(cur):
-    cur.execute("select name, premiered from tvshows order by premiered")
-    tab = cur.fetchall()
-    try:
-        tab = np.array(tab)
-        df = pd.DataFrame(data=tab, columns=["name", "premiered"])
-        print(df)
-    except ValueError:
-        print("Таблица пуста")
 
 
 def insert_into_table(cur, data):
@@ -28,22 +15,16 @@ def insert_into_table(cur, data):
 
 if __name__ == '__main__':
     with psycopg2.connect(dbname="postgres", user="postgres", password="example") as conn:
-        menue = input(f"Введите нужный пункт меню \n\tПоиск \n\tВывод \n\tСтоп\nОпция:")
         cur = conn.cursor()
-        while menue != "Стоп":
-            if menue == "Поиск":
-                tv_show = input("Поиск: ")
-                req = requests.get(TVSHOW + tv_show)
-                response = req.json()
-                if len(response) > 0:
-                    new_show = {'id': response[0]['show']['id'],
-                                'name': response[0]['show']['name'],
-                                'url': response[0]['show']['url'],
-                                'premiered': dateparser.parse(response[0]['show']['premiered']),
-                                'status': response[0]['show']['status']}
+        tv_show = input("Поиск: ")
+        req = requests.get(TVSHOW + tv_show)
+        response = req.json()
+        if len(response) > 0:
+            new_show = {'id': response[0]['show']['id'],
+                        'name': response[0]['show']['name'],
+                        'url': response[0]['show']['url'],
+                        'premiered': dateparser.parse(response[0]['show']['premiered']),
+                        'status': response[0]['show']['status']}
 
-                insert_into_table(cur, list(new_show.values()))
-            elif menue == "Вывод":
-                print_table(cur)
-            menue = input(f"Введите нужный пункт меню \n\tПоиск \n\tВывод \n\tСтоп\nОпция:")
+            insert_into_table(cur, list(new_show.values()))
         cur.close()
